a9114b449bacdd4999cce1493ce39ec9f03b3ae5
Mingfeng Mai <mingf.m04@gmail.com>
2022-11-11 21:05:35 -0500

    get shopping cart info

--- controllers/shoppingcart.js
+++ controllers/shoppingcart.js
@@ -1,3 +1,35 @@
-module.exports = {
+const userController = require('./user')
 
+const shoppingcart = {
+    async getinfo(ctx) {
+        let { session, service } = ctx
+
+        if (!session.userLogged) {
+            ctx.error('You have not logged in yet', 403)
+            return null
+        }
+
+        const userId = session.userId
+
+        let cart = await service.db.collection('cart').find({
+            user_id: userId
+        })
+
+        if(!cart) {
+            return []
+        }
+
+        return cart
+    },
+// cart struct in db
+    // userid
+    // list of items with items id
+    // list of item's capacity
+    // list of item's price in single item
+    async addItem(user) {
+
+    }
 }
+
+
+module.exports = shoppingcart
--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const Router = require('@koa/router');
 const home = require('./controllers/home')
 const auth = require('./controllers/auth')
 const user = require('./controllers/user')
+const shoppingcart = require('./controllers/shoppingcart')
 
 const router = new Router();
 router.get('/', home.home)
@@ -14,5 +15,8 @@ router.get('/api/auth/loginStatus', auth.loginStatus)
 router.get('/api/user/userInfo', user.userInfo)
 router.post('/api/user/userUpdateInfo', user.userUpdateInfo)
 router.post('/api/user/changePassword', user.changePassword)
+
+router.get('/api/shoppingcart/getinfo', shoppingcart.getinfo)
+router.post('/api/shoppingcart/addItem', shoppingcart.addItem())
 
 module.exports = router
