bb753fed427e39f90ffd875a45c8890eddcdd471
Jiahao Guo <1147678303@qq.com>
2022-11-14 15:38:38 -0500

    add static

--- utils/staticFiles.js
+++ utils/staticFiles.js
@@ -1,15 +1,18 @@
 let pathTools = require('./pathTools')
-const path = require("path");
+const path = require("path")
+const fs = require('fs')
+const mimeTypes = require('mime-types')
 
 module.exports = (root) => {
     return async (ctx, next) => {
         await next()
-
-        console.log('here', ctx.body, ctx.statusCode)
-        console.log(ctx)
         if (ctx.method === 'GET' && !ctx.body && ctx.status === 404) {
-            console.log(ctx.url)
-            let path = path.join(root, utils.normalizePath(request.rawUrl))
+            let filePath = path.join(root, pathTools.normalizePath(ctx.url))
+
+            if(fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory()) {
+                ctx.type = mimeTypes.contentType(path.extname(filePath))
+                ctx.body = fs.createReadStream(filePath)
+            }
         }
     }
 }
